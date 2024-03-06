ae5645ef842a77ddb8ad6de9e8974ce1057b97ae
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-03-06 23:15:17 +0800

    update ulit sa color

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "#006400",
+          DEFAULT: "#008000",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
