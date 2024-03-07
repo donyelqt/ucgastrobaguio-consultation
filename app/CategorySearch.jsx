ef3f8049ea16cf2c4c4ebc54c4e9e251c28f6a41
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-03-07 10:42:28 +0800

    update

--- app/CategorySearch.jsx
+++ app/CategorySearch.jsx
@@ -6,13 +6,15 @@ import { MapPinned, Search, SearchCheckIcon } from 'lucide-react'
 function CategorySearch() {
   return (
     <div className='mb-10 items-center flex flex-col gap-2 mt-24'>
-        <div className="flex w-full mt-3 max-w-sm items-center space-x-2">
+        <h2 className='font-bold text-4xl tracking-wide'><span className='text-primary'style={{ fontFamily: '"Lucida Fax Demi", serif' }}>Search Hospitals</span><span style={{ fontFamily: '"Lucida Fax Demi", serif' }}> in Baguio City, Philippines</span></h2>
+        <h2 className='text-gray-500 text-xl'></h2>
+
+        <div className="flex w-full mt-3 max-w-lg items-center space-x-2">
       <Input type="text" placeholder="Search Now Here!!!" style={{ borderRadius: '20px' }}/>
       <Button type="submit" style={{ borderRadius: '20px' }}>
         <SearchCheckIcon className='h-4 w-4 mr-1'/>
         Search</Button>
     </div>
-        <h2 className='font-bold text-4xl tracking-wide mt-5'><span className='text-primary'style={{ fontFamily: '"Lucida Fax Demi", serif' }}>Search Hospitals</span><span style={{ fontFamily: '"Lucida Fax Demi", serif' }}> in Baguio City, Philippines</span></h2>
     </div>
   )
 }
