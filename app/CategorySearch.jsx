3b41cd54deb987eae48503f3b85b77cef2f7a1cf
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-03-07 10:48:33 +0800

    update

--- app/CategorySearch.jsx
+++ app/CategorySearch.jsx
@@ -9,7 +9,7 @@ function CategorySearch() {
         <h2 className='font-bold text-4xl tracking-wide'><span className='text-primary'style={{ fontFamily: '"Lucida Fax Demi", serif' }}>Search Hospitals</span><span style={{ fontFamily: '"Lucida Fax Demi", serif' }}> in Baguio City, Philippines</span></h2>
         <h2 className='text-gray-500 text-xl'></h2>
 
-        <div className="flex w-full mt-3 max-w-lg items-center space-x-2">
+        <div className="flex w-full mt-3 max-w-sm items-center space-x-2">
       <Input type="text" placeholder="Search Now Here!!!" style={{ borderRadius: '20px' }}/>
       <Button type="submit" style={{ borderRadius: '20px' }}>
         <SearchCheckIcon className='h-4 w-4 mr-1'/>
