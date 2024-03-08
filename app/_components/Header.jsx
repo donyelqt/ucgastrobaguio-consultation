2e1e962d5d94cc6493a70050e4b8fa57b357faa2
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-03-08 20:44:41 +0800

    change the layout of navbar

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -4,40 +4,44 @@ import Link from 'next/link'
 import React from 'react'
 
 function Header() {
-    const Menu=[
+    const Menu = [
         {
-            id:1,
-            name:'Home',
-            path:'/'
+            id: 1,
+            name: 'Home',
+            path: '/'
         },
         {
-            id:1,
-            name:'Services',
-            path:'/'
+            id: 2,
+            name: 'Services',
+            path: '/'
         },
         {
-            id:1,
-            name:'Hospitals',
-            path:'/'
+            id: 3,
+            name: 'Hospitals',
+            path: '/'
         },
-    ]
-  return (
-    <div className='flex items-center justify-between p-4 shadow-lg'>
-        <div className='flex items-center gap-10 mx-8'>
-            <Image src='/uclogonew.png' alt="logo" 
-            width={80} height={80}
-            />
-           <ul className='md:flex gap-8 hidden mx-8'>
-               {Menu.map((item,index)=>(
-                   <Link href={item.path}>
-                   <li className='hover:text-primary cursor-pointer hover:scale-110 transition-all ease-in-out'>{item.name}</li>
-                   </Link>
-               ))}
-           </ul>
+    ];
+    return (
+        <div className='flex items-center justify-between p-4 shadow-lg'>
+            <div className='flex items-center gap-4 mx-10'>
+                <Image src='/uclogonew.png' alt="logo"
+                    width={80} height={80} style={{ margin: '0 20px' }}
+                />
+            </div>
+            <div className="flex items-center gap-8">
+                <ul className='md:flex gap-4 hidden mx-1'>
+                    {Menu.map((item, index) => (
+                        <Link href={item.path} key={item.id}>
+                            <li className='hover:text-primary cursor-pointer hover:scale-110 transition-all ease-in-out' style={{ margin: '0 20px' }}>
+                                {item.name}
+                            </li>
+                        </Link>
+                    ))}
+                </ul>
+                <Button className='mx-10 hover:bg-black' style={{ borderRadius: '20px', marginLeft: '20px' }}>Log In Now Here!</Button>
+            </div>
         </div>
-        <Button className='mx-8 hover:bg-black' style={{ borderRadius: '20px' }}>Log In Now Here !!!</Button>
-    </div>
-  )
+    );
 }
 
-export default Header 
+export default Header;
