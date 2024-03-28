beaef1d493ecfa1108c6742b13f0e40e0f53a178
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-03-28 21:18:54 +0800

    update

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -18,7 +18,7 @@ function Hero() {
       </div>
 
       <div className="lg:py-24">
-        <h2 className="text-3xl font-bold sm:text-4xl"><span className='text-primary'style={{ fontFamily: '"Lucida Fax Demi", serif' }}>UC GastroBaguio</span><span style={{ fontFamily: '"Lucida Fax Demi", serif' }}> - Consultation</span></h2>
+        <h2 className="text-3xl font-bold sm:text-3xl"><span className='text-primary'style={{ fontFamily: '"Lucida Fax Demi", serif' }}>UC GastroBaguio</span><span style={{ fontFamily: '"Lucida Fax Demi", serif' }}> - Consultation System</span></h2>
 
         <p className="mt-4 text-gray-600">
            Welcome to the University of the Cordilleras GastroBaguio - Consultation System! You can now schedule your consultation with the hospitals based in Baguio City, Philippines, quickly and efficiently on this online platform!
