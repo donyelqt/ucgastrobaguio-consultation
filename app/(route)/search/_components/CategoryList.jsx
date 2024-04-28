d68aa6cc68d9e4f73a9e45ce7ddf866132a38ac0
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-04-29 02:14:40 +0800

    new search bar

--- app/(route)/search/_components/CategoryList.jsx
+++ app/(route)/search/_components/CategoryList.jsx
@@ -1,6 +1,18 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import GlobalApi from '@/app/_utils/GlobalApi'
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command"
+import Link from 'next/link';
 
 const categoryList1 = [
   {
@@ -27,7 +39,26 @@ function CategoryList() {
     })
   }
   return (
-    <div>CategoryList</div>
+    <div className='h-screen fixed mt-5 flex flex-col'>
+      <Command>
+  <CommandInput placeholder="Type a command or search..." />
+  <CommandList className="overflow-visible">
+    <CommandEmpty>No results found.</CommandEmpty>
+    <CommandGroup heading="Suggestions">
+      {categoryList1&&categoryList1.map((item,index)=>(
+        <CommandItem>
+          <Link href={''}
+          className='p-2 flex gap-2 text-[12px] text-green-600 rounded-md cursor-pointer w-full'>
+            <img src={item.imageUrl} alt={item.name} width={40} height={40} />
+            <label>{item.name}</label>
+          </Link>
+        </CommandItem>
+      ))}
+    </CommandGroup>
+   
+  </CommandList>
+</Command>
+    </div>
   )
 }
 
